7239845480191c5b016c30f5a9fe6bfbacaeea8e
Manfred Mueller-Spaeth <fms1961@gmail.com>
2018-07-07 19:03:52 +0200

    Lib changes

--- src/ssd1306_i2c.c
+++ src/ssd1306_i2c.c
@@ -665,6 +665,7 @@ void mgos_ssd1306_rotate_display (struct mgos_ssd1306 *oled, bool alt)
   if (oled == NULL)
     return;
 
+	//  a little hack: the commands must be delayed
 	if (alt) {
 	  _command (oled, 0xA1);
 	  mgos_msleep(150);
@@ -685,8 +686,11 @@ void mgos_ssd1306_flip_display (struct mgos_ssd1306 *oled, bool horizontal, bool
 
   uint8_t compins = oled->height < 64 ? 0x02 : 0x12;
   _command (oled, 0xda);
+	  mgos_msleep(150);
   _command (oled, compins | (horizontal << 5));
+	  mgos_msleep(150);
   _command (oled, vertical ? 0xc0 : 0xc8);
+	  mgos_msleep(150);
 }
 
 void mgos_ssd1306_update_buffer (struct mgos_ssd1306 *oled, uint8_t * data, uint16_t length)
